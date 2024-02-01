03cf3b0490b723ad8dbe37dabe15aebb9fdd2d41
RetoStutz <retostutz@yahoo.de>
2024-02-01 23:17:00 +0100

    import sap data

--- src/database/database.py
+++ src/database/database.py
@@ -32,20 +32,23 @@ def import_data_from_polarion_task(con, cursor, file_path):
                 # Insert into database based on 'Type'
                 if row.get('Type') == 'User Story':
                     parent_id = find_number_in_string(row.get('Linked Work Items'), "specifies:")
+                    scc_id = extract_number_from_string(row['ID'])
                     cursor.execute(
                         "INSERT INTO Story (sccId, name, parentSccId) VALUES (?, ?, ?)",
-                        (row['ID'], row['Title'], parent_id))
+                        (scc_id, row['Title'], parent_id))
 
                 elif row.get('Type') == 'Epic':
+                    scc_id = extract_number_from_string(row['ID'])
                     cursor.execute(
                         "INSERT INTO Epic (sccId, name) VALUES (?, ?)",
-                        (row['ID'], row['Title']))
+                        (scc_id, row['Title']))
 
                 elif row.get('Type') == 'Task':
                     parent_id = find_number_in_string(row.get('Linked Work Items'), "implements:")
+                    scc_id = extract_number_from_string(row['ID'])
                     cursor.execute(
                         "INSERT INTO Task (sccId, name, parentSccId) VALUES (?, ?, ?)",
-                        (row['ID'], row['Title'], parent_id))
+                        (scc_id, row['Title'], parent_id))
                 else:
                     print(f"{row.get('Type')} is not part of database")
 
@@ -53,6 +56,30 @@ def import_data_from_polarion_task(con, cursor, file_path):
     except Exception as e:
         print(f"Error during import data from {file_path}: {str(e)}")
 
+
+def import_data_from_sap(con, cursor, file_path):
+    try:
+        # read data from excel
+        excel_data = pd.read_excel(file_path, sheet_name=None)
+
+        for sheet_name, data in excel_data.items():
+            # Iterate over rows and insert into SQLite table
+            for index, row in data.iterrows():
+
+                # Insert into database based on 'Type'
+                if isinstance(row['Short Text'], str):
+                    scc_id = find_first_alphanumeric_sequence(row['Short Text'])
+                    if scc_id != 0:
+                        cursor.execute(
+                            "INSERT INTO Workload (sccId, numberOfHours, worker) VALUES (?, ?, ?)",
+                            (scc_id, row['Number (unit)'], row['Name of employee or applicant']))
+
+                else:
+                    print(f"no short text written")
+
+            con.commit()
+    except Exception as e:
+        print(f"Error during import data from {file_path}: {str(e)}")
 
 def find_number_in_string(searchable_string, key_word):
     if isinstance(searchable_string, str):
@@ -68,6 +95,18 @@ def find_number_in_string(searchable_string, key_word):
     else:
         return 0
 
+
+def extract_number_from_string(searchable_string):
+    return int(re.findall(r'\d+', searchable_string)[0])  # Use \d+ to find all digits
+
+
+def find_first_alphanumeric_sequence(input_string):
+    match = re.search(r'\b[A-Za-z]+-(\d+)', input_string)
+    if match:
+        numeric_part = match.group(1)
+        return int(numeric_part)
+    else:
+        return 0
 
 class Database:
 
@@ -117,12 +156,23 @@ class Database:
             )
         ''')
 
+        # Create Table Workload
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Workload (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                sccId INTEGER,
+                numberOfHours INTEGER,
+                worker TEXT
+            )
+        ''')
+
         # Commit the changes
         self.conn.commit()
 
         # import data from excel tables
         if IMPORT_DATA_FROM_FILE:
             import_data_from_polarion_task(self.conn, self.cursor, excel_file_path_tracker_list)
+            import_data_from_sap(self.conn, self.cursor, excel_file_path_sap)
 
         # close connection
         self.conn.close()
